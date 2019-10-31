5a610291bdab0383e305b07be574f76499974c3c
Lauren <lauren.c4231@gmail.com>
2019-10-31 14:40:09 +0000

    Add global styles and query/fallback for CSSGrid

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <template>
-  <main id="app">
-    <Cars />
-  </main>
+    <main role="main" id="app">
+        <Cars />
+    </main>
 </template>
 
 <script>
 import Cars from "./components/Cars.vue";
 
 export default {
-  name: "app",
-  components: {
-    Cars
-  }
+    name: "app",
+    components: {
+        Cars
+    }
 };
 </script>
 
 <style lang="scss">
+* {
+    box-sizing: border-box;
+}
+body {
+    padding: 1rem;
+}
+
+main {
+    max-width: 500px;
+    margin: 0 auto;
+}
+article {
+    margin: 1rem 0;
+}
+
+@supports (display: grid) {
+    main {
+        max-width: 10000px;
+        margin: 0;
+    }
+    article {
+        margin: 0;
+    }
+    .container {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(400px, 2fr));
+        grid-gap: 1.3rem;
+    }
+}
 </style>
